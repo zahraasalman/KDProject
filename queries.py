2e87aac08bc6374960d1edf8f19f7ac731a6b4d6
Sara Prifti <sara.prifti@pxlwidgets.com>
2020-10-25 12:38:28 +0100

    Squashed commit of the following:
    commit 8df07eb5c7f8e1d9b71901e0577b17807bf04e89
    Author: Zahraa Salman <zahraa-salman@hotmail.com>
    Date:   Sun Oct 25 06:05:57 2020 +0100
    
        add layout and map
    
    commit f843db3dba2cfb52706937b584c5112cd2eeb3e3
    Author: Zahraa Salman <zahraa-salman@hotmail.com>
    Date:   Sat Oct 24 22:42:48 2020 +0200
    
        merge with Experimental-map-bugfixing
    
    commit 3660525c176a79e2da960d9dbfc355aae7e2098e
    Merge: c314b6b 2ffc4fa
    Author: Zahraa Salman <zahraa-salman@hotmail.com>
    Date:   Sat Oct 24 21:52:02 2020 +0200
    
        Merge branch 'Experimental-map-bugfixing' of github.com:/zahraasalman/KDProject into unworthy_master
    
         Conflicts:
        	index.py
        	map.py
        	queries.py
    
    commit c314b6bc8c1eff4c5649fe3d4ffb02f0788ccd02
    Author: Zahraa Salman <zahraa-salman@hotmail.com>
    Date:   Sat Oct 24 21:07:43 2020 +0200
    
        add labels
    
    commit 2ffc4fa6dc1aff0f5bedd5969ed8a421cad23210
    Author: MichM072 <michaelsmoor@outlook.com>
    Date:   Sat Oct 24 19:43:44 2020 +0200
    
        end me
    
    commit 6d4617e879bd5e15a8bda8546e84a1c739350daa
    Author: MichM072 <michaelsmoor@outlook.com>
    Date:   Sat Oct 24 17:58:20 2020 +0200
    
        map always on display / button no function
    
    commit 62f1242a1045f5491a4bc93efc5a59d88ddd5f2e
    Author: MichM072 <michaelsmoor@outlook.com>
    Date:   Fri Oct 23 23:08:52 2020 +0200
    
        still borked but less borked
    
    commit 6e23725033fda5183ebea70615a2d67ac2f687a4
    Author: MichM072 <michaelsmoor@outlook.com>
    Date:   Fri Oct 23 00:48:09 2020 +0200
    
        Borked variant for mexico

--- queries.py
+++ queries.py
@@ -3,11 +3,7 @@ import SPARQLWrapper
 from rdflib import Graph, RDF, Namespace, Literal, URIRef
 from SPARQLWrapper import SPARQLWrapper, JSON
 
-
-# sparql = SPARQLWrapper("https://kd-project.vercel.app/ontologyWithData.ttl")
-# sparql = SPARQLWrapper("https://kd-project.vercel.app/statements.rj")
-# sparql = SPARQLWrapper("https://projectkd-a951ufu8j.vercel.app/graph.json")
-sparql = SPARQLWrapper("http://192.168.1.110:7200/repositories/FinalProject")
+sparql = SPARQLWrapper("http://192.168.1.103:7200/repositories/FinalProject")
 
 
 def get_continents():
@@ -15,57 +11,58 @@ def get_continents():
     sparql.setQuery("""
         PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
         PREFIX : <http://www.tourism.org/group6/>
-        select ?continent where { 
-            ?continent rdf:type :Continent
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        select * where { 
+            ?continent rdf:type :Continent;
+                        rdfs:label ?continentlabel.
         }
         """)
     sparql.setReturnFormat(JSON)
     results = sparql.query().convert()
 
     for result in results["results"]["bindings"]:
-        result = result["continent"]["value"].split('/')[-1]
-        result = result.replace("%20", " ")
-        result_list.append(result)
+        continent = result["continent"]["value"].split('/')[-1]
+        continentlabel = result["continentlabel"]["value"]
 
-    if result_list == []:
-        result_list = "No results found, please try another option!"
+        result_list.append((continent, continentlabel))
+
     return result_list
 
 
 def get_regions(continent='all'):
     result_list = []
-    continent = continent.replace(" ", "%20")
 
     if continent == 'all':  # continent is not selected so show all regions
         sparql.setQuery("""
             PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
             PREFIX : <http://www.tourism.org/group6/>
-            select ?region where { 
+            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+            select distinct ?region ?regionlabel where { 
                 ?country rdf:type :Country;
                          :fromContinent ?continent;
-                         :fromRegion ?region.        
-            }GROUP BY (?region)
+                         :fromRegion ?region.      
+                ?region rdfs:label ?regionlabel.  
+            }
             """)
-    else: # continent is selected
+    else:  # continent is selected
         sparql.setQuery("""
                 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
                 PREFIX : <http://www.tourism.org/group6/>
-                select ?region where { 
+                PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+                select distinct ?region ?regionlabel where { 
                 ?country rdf:type :Country;
                          :fromContinent :%s;
-                         :fromRegion ?region.        
-                }GROUP BY (?region)
+                         :fromRegion ?region.   
+                ?region rdfs:label ?regionlabel.       
+                }
                 """ % continent)
     sparql.setReturnFormat(JSON)
     results = sparql.query().convert()
 
     for result in results["results"]["bindings"]:
-        result = result["region"]["value"].split('/')[-1]
-        result = result.replace("%20", " ")
-        result_list.append(result)
-
-    if result_list == []:
-        result_list = "No results found, please try another option!"
+        region = result["region"]["value"].split('/')[-1]
+        regionlabel = result["regionlabel"]["value"]
+        result_list.append((region, regionlabel))
     return result_list
 
 
@@ -78,16 +75,20 @@ def get_countries(continent='all', region='all'):
         sparql.setQuery("""
                 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
                 PREFIX : <http://www.tourism.org/group6/>
-                select ?country where { 
-                    ?country rdf:type :Country
+                PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+                select ?country ?countrylabel where { 
+                    ?country rdf:type :Country;
+                            rdfs:label ?countrylabel.
                 }
                 """)
     elif continent != 'all' and region == 'all':  # continent is selected but not region
         sparql.setQuery("""
                         PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
                         PREFIX : <http://www.tourism.org/group6/>
-                        select ?country where { 
+                        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+                        select ?country ?countrylabel where { 
                             ?country rdf:type :Country;
+                                    rdfs:label ?countrylabel;
                                     :fromContinent :%s.
                         }
                         """ % continent)
@@ -95,8 +96,10 @@ def get_countries(continent='all', region='all'):
         sparql.setQuery("""
                 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
                 PREFIX : <http://www.tourism.org/group6/>
-                select ?country where { 
+                PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+                select ?country ?countrylabel where { 
                     ?country rdf:type :Country;
+                            rdfs:label ?countrylabel;
                             :fromRegion :%s.
                         }
                 """ % region)
@@ -104,8 +107,10 @@ def get_countries(continent='all', region='all'):
         sparql.setQuery("""
                         PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
                         PREFIX : <http://www.tourism.org/group6/>
-                        select ?country where { 
+                        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+                        select ?country ?countrylabel where { 
                             ?country rdf:type :Country;
+                                    rdfs:label ?countrylabel;
                                     :fromContinent :%s;
                                     :fromRegion :%s.
                         }
@@ -115,12 +120,10 @@ def get_countries(continent='all', region='all'):
     results = sparql.query().convert()
 
     for result in results["results"]["bindings"]:
-        result = result["country"]["value"].split('/')[-1]
-        result = result.replace("%20", " ")
-        result_list.append(result)
+        country = result["country"]["value"].split('/')[-1]
+        countrylabel = result["countrylabel"]["value"]
+        result_list.append((country, countrylabel))
 
-    if result_list == []:
-        result_list = "No results found, please try another option!"
     return result_list
 
 
@@ -131,9 +134,10 @@ def get_capitals(country):  # could be changed to get cities
     sparql.setQuery("""
                     PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
                     PREFIX : <http://www.tourism.org/group6/>
-                    select ?capital where { 
-                        ?capital rdf:type :Capital;
-                                :fromCountry :%s . 
+                    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+                    select ?capital ?capitallabel where { 
+                        :%s :hasCapital ?capital.
+                        ?capital rdfs:label ?capitallabel.
                     } 
                     """ % country)
 
@@ -141,27 +145,49 @@ def get_capitals(country):  # could be changed to get cities
     results = sparql.query().convert()
 
     for result in results["results"]["bindings"]:
-        result = result["capital"]["value"].split('/')[-1]
-        result = result.replace("%20", " ")
-        result_list.append(result)
+        capital = result["capital"]["value"].split('/')[-1]
+        capitallabel = result["capitallabel"]["value"]
+        result_list.append((capital, capitallabel))
 
-    if result_list == []:
-        result_list = "No results found, please try another option!"
     return result_list
 
 
 def get_country_coordinates(country):
+    # The cordinates are fucked, probably a faulty css or an import error from GraphDB
+    # Mexico is the only working version, the rest is borked, wil investigate tomorrow. (23 okt)
     result_list = []
-    country = country.replace(" ", "%20")
 
     sparql.setQuery("""
                     PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
                     PREFIX : <http://www.tourism.org/group6/>
                     select * where { 
-                            :%s :hasLatitude ?lat;
-                                :hasLongitude ?long.
+                            :%s :hasCoordinates ?cordinates.
                     } 
                     """ % country)
+
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+    for result in results["results"]["bindings"]:
+        result1 = str(result["cordinates"]["value"])
+        if [result1] not in result_list:
+            result_list.append([result1])
+
+    return result_list
+
+def get_capital_coordinates(capital):
+    result_list = []
+    capital = capital.replace(" ", "%20")
+
+    sparql.setQuery("""
+                    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                    PREFIX : <http://www.tourism.org/group6/>
+                    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+                    select * where { 
+                            :%s rdf:type :Country;
+                                :hasLatitude ?lat;
+                                :hasLongitude ?long.
+                    } 
+                    """ % capital)
 
     sparql.setReturnFormat(JSON)
     results = sparql.query().convert()
@@ -171,8 +197,6 @@ def get_country_coordinates(country):
         if [result1, result2] not in result_list:
             result_list.append([result1, result2])
 
-    if result_list == []:
-        result_list = "No results found, please try another option!"
     return result_list
 
 
@@ -181,4 +205,4 @@ def get_country_coordinates(country):
 # print(get_countries(region="Southern%20Europe"))
 # print(get_regions("Europe"))
 # print(get_capitals("Netherlands"))
-print(get_country_coordinates("Mexico"))
+# print(get_country_coordinates("Mexico"))

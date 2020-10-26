06d13006ab4290f914e2be863f250f4e61d2f5fa
Sara Prifti <sara.prifti@pxlwidgets.com>
2020-10-26 04:34:43 +0100

    added info city page

--- city_page.py
+++ city_page.py
@@ -1,10 +1,45 @@
 import streamlit as st
+import queries as Q
 
 
 def show(city=None):
     st.title(city)
-    st.write("""
-    
-    information about the city
-    
-    """)
+
+    ### QUERIES ###
+    landmarks = Q.get_city_landmarks(city)
+    theaters = Q.get_city_theaters(city)
+    markets = Q.get_city_markets(city)
+    themeParks = Q.get_city_theme_parks(city)
+    parks = Q.get_city_parks(city)
+
+    st.markdown('** Info **')
+
+    if landmarks:
+        st.write(''' Landmarks:  ''')
+
+        for landmark in landmarks:
+            st.write(landmark + ", ")
+
+    if theaters:
+        st.write(''' Theaters:  ''')
+
+        for theater in theaters:
+            st.write(theaters + ", ")
+
+    if markets:
+        st.write(''' Markets:  ''')
+
+        for market in markets:
+            st.write(markets + ", ")
+
+    if parks:
+        st.write(''' Parks:  ''')
+
+        for park in parks:
+            st.write(park + ", ")
+
+    if themeParks:
+        st.write(''' Theme Parks:  ''')
+
+        for themePark in themeParks:
+            st.write(themePark + ", ")

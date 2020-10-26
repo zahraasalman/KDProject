6c23b041e4745eb88b2922b8fe6c7a0c07ec996c
Sara Prifti <sara.prifti@pxlwidgets.com>
2020-10-26 04:10:37 +0100

    added files country and city page py

---
+++ city_page.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+def show(city=None):
+    st.title(city)
+    st.write("""
+    
+    information about the city
+    
+    """)
---
+++ country_page.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+def show(country=None):
+    st.title(country)
+    st.write("""
+
+       information about the country
+
+       """)
